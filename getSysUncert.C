1fc958d9038024a1adfe75ba2079f3856c5a35aa
Yongsun <kingmking@gmail.com>
2013-09-30 23:16:47 +0900

    systematic uncertatiny by energy scale

--- getSysUncert.C
+++ getSysUncert.C
@@ -17,9 +17,9 @@ void getSysUncert() {
  
   for ( int ifile = 0 ; ifile <= 1 ; ifile++ ) { 
     if ( ifile == 0 ) 
-      f = new TFile("resultHistograms_MC_recoIsoPhoton.root");
+      f = new TFile("resultHistograms.root");
     else 
-      f = new TFile("resultHistograms_MC_genIsoPhoton.root");
+      f = new TFile("resultHistograms_photonEnergy_ScaledBy0.015.root");
     
     for ( int coll = 1 ; coll<=3 ; coll++) {   // On Sep 30, only pp and pbpb is studied.  pA will be added very soon
       TString collName;
@@ -58,7 +58,7 @@ void getSysUncert() {
   }
   cout <<"  2 " << endl;
   
-  TFile * fSysResults = new TFile("relativeSys_dueTo_isolation.root","update");
+  TFile * fSysResults = new TFile("relativeSys_dueTo_energyScalePlus.root","update");
   for ( int coll = 1 ; coll<=3 ; coll++) {   // On Sep 30, only pp and pbpb is studied.  pA will be added very soon        
     for ( int ipt = 1 ; ipt <=nPtBin ; ipt++) {
       dNdJetPt[coll][ipt][2]->Write();
