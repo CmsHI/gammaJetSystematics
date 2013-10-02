758f9d537ffe1453632689ac7ae3efda95188ce2
Yongsun <kingmking@gmail.com>
2013-10-02 23:21:57 +0900

    getSysUncert.C updated

--- getSysUncert.C
+++ getSysUncert.C
@@ -18,9 +18,9 @@ void getSysUncert() {
   for ( int ifile = 0 ; ifile <= 1 ; ifile++ ) { 
 
     if ( ifile == 0 ) 
-      f = new TFile("resultHistograms_MC_genIsoPhoton.root");
+      f = new TFile("resultHistograms.root");
     else 
-      f = new TFile("resultHistograms_MC_recoIsoPhoton.root");
+      f = new TFile("resultHistograms_energySmearedBy10percent.root");
     
     for ( int coll = 1 ; coll<=4 ; coll++) {   // On Sep 30, only pp and pbpb is studied.  pA will be added very soon
       TString collName;
@@ -37,7 +37,6 @@ void getSysUncert() {
       }
     }
   }
-  cout <<"  1 " << endl;
   // relative uncertainty calculation
   for ( int coll = 1 ; coll<=4 ; coll++) {   // On Sep 30, only pp and pbpb is studied.  pA will be added very soon                                                
     for ( int ipt = 1 ; ipt <=nPtBin ; ipt++) {
@@ -54,9 +53,12 @@ void getSysUncert() {
     
   }
   
-  //  TFile * fSysResults = new TFile("relativeSys_dueTo_energyScalePlus.root","recreate");
-  //TFile * fSysResults = new TFile("relativeSys_dueTo_energyScaleMinus.root","recreate");
-  TFile * fSysResults = new TFile("relativeSys_dueTo_isolation.root","recreate");
+  // TFile * fSysResults = new TFile("relativeSys_dueTo_energyScalePlus.root","recreate");
+  // TFile * fSysResults = new TFile("relativeSys_dueTo_energyScaleMinus.root","recreate");
+  
+
+  TFile * fSysResults = new TFile("relativeSys_dueTo_jetResolution.root","recreate");
+  
   for ( int coll = 1 ; coll<=4 ; coll++) {   // On Sep 30, only pp and pbpb is studied.  pA will be added very soon        
     for ( int ipt = 1 ; ipt <=nPtBin ; ipt++) {
       dNdJetPt[coll][ipt][2]->Write();
@@ -66,7 +68,7 @@ void getSysUncert() {
   }
   
   fSysResults->Close();
-
+  
   
 }
  
