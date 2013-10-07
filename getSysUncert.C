67fcbf1d741af3f030dfeb6a9b7c838e1a675e28
Yongsun <kingmking@gmail.com>
2013-10-07 17:08:37 +0900

    used to get the uncertainty root file

--- getSysUncert.C
+++ getSysUncert.C
@@ -2,27 +2,36 @@
 #include <TRandom3.h>
 #include <Tfile.h>
 
-void getSysUncert() {
-  cout << "hello" << endl;
+void getSysUncert(   
+		  TString fname1 = "resultHistograms.root", 
+		  TString fname2 = "resultHistograms_jetEnegyScalePlus.root"
+		     )
+{
+  cout << "===============================================================================" << endl;
+  cout << " Comparing " << fname1 <<"   VS  " << fname2 << endl;
+  cout << "===============================================================================" << endl;
+  
   const int kPP = 1;
   const int kHIcentral =  2;
   const int kHIperipheral = 3;
   const int kPA = 4;
   const int nPtBin = 4;  
   
-  TH1D* dNdJetPt[4][5][3];  // [collision] [ ptbin]  [Before/After variation]
-  TH1D* meanJetPt[4][3];   // [collision] [Before/After variation]
-  TH1D* meanRjg[4][3];
+  TH1D* dNdJetPt[5][5][3];  // [collision] [ ptbin]  [Before/After variation]
+  TH1D* dNdXjg[5][5][3];  // [collision] [ ptbin]  [Before/After variation]                                                                                 
+  TH1D* meanXjg[5][3];   // [collision] [Before/After variation]
+  TH1D* meanJetPt[5][3];   // [collision] [Before/After variation]
+  TH1D* meanRjg[5][3];
   TFile* f; 
  
   for ( int ifile = 0 ; ifile <= 1 ; ifile++ ) { 
 
     if ( ifile == 0 ) 
-      f = new TFile("resultHistograms.root");
+      f = new TFile(fname1.Data());
     else 
-      f = new TFile("resultHistograms_photonEnergy_ScaledBy0.015.root");
+      f = new TFile(fname2.Data());
 
-    for ( int coll = 1 ; coll<=4 ; coll++) {   // On Sep 30, only pp and pbpb is studied.  pA will be added very soon
+    for ( int coll = 1 ; coll<=4 ; coll++) { 
       TString collName;
       if ( coll == kPP )  collName = "pp";
       else if ( coll == kPA )  collName = "ppb";
@@ -30,40 +39,51 @@ void getSysUncert() {
       else if ( coll == kHIperipheral )  collName = "pbpb_centralityBin2";
       
       meanJetPt[coll][ifile]  =(TH1D*)f->Get(Form("meanJetPt_%s", collName.Data()));
+      meanXjg[coll][ifile]  =(TH1D*)f->Get(Form("meanXjg_%s", collName.Data()));
       meanRjg[coll][ifile]  =(TH1D*)f->Get(Form("meanRjg_%s", collName.Data()) );
       
       for ( int ipt = 1 ; ipt <=nPtBin ; ipt++) { 
 	dNdJetPt[coll][ipt][ifile] = (TH1D*)f->Get(Form("dNdJetPt_IaaBin_%s_ptBin%d", collName.Data(), ipt ) );
+	dNdXjg[coll][ipt][ifile] = (TH1D*)f->Get(Form("dNdXjg_%s_ptBin%d", collName.Data(), ipt ) );
+	//	cout << Form("dNdXjg_%s_ptBin%d", collName.Data(), ipt )  << endl;
       }
     }
   }
-  // relative uncertainty calculation
-  for ( int coll = 1 ; coll<=4 ; coll++) {   // On Sep 30, only pp and pbpb is studied.  pA will be added very soon                                                
+  
+  for ( int coll = 1 ; coll<=4 ; coll++) {   
     for ( int ipt = 1 ; ipt <=nPtBin ; ipt++) {
       dNdJetPt[coll][ipt][2] = (TH1D*)dNdJetPt[coll][ipt][1]->Clone(Form("%s_uncertainty", dNdJetPt[coll][ipt][1]->GetName()));
       dNdJetPt[coll][ipt][2]->Divide( dNdJetPt[coll][ipt][0] );
+
+      //    cout << " here 1 " << endl;
+      dNdXjg[coll][ipt][2] = (TH1D*)dNdXjg[coll][ipt][1]->Clone(Form("%s_uncertainty", dNdXjg[coll][ipt][1]->GetName()));
+      //    cout << " here 2 " << endl;
+      dNdXjg[coll][ipt][2]->Divide( dNdXjg[coll][ipt][0] );
     }
     meanJetPt[coll][2] = (TH1D*)meanJetPt[coll][1]->Clone(Form("%s_uncertainty", meanJetPt[coll][1]->GetName()) );
     meanJetPt[coll][2]->Divide( meanJetPt[coll][0] );
+
+    meanXjg[coll][2] = (TH1D*)meanXjg[coll][1]->Clone(Form("%s_uncertainty", meanXjg[coll][1]->GetName()) );
+    meanXjg[coll][2]->Divide( meanXjg[coll][0] );
      
-    cout << " coll = " << coll << endl;
-    cout << " name = " << meanRjg[coll][1]->GetName() << endl;
+    //  cout << " coll = " << coll << endl;
+    //  cout << " name = " << meanRjg[coll][1]->GetName() << endl;
     meanRjg[coll][2] = (TH1D*)meanRjg[coll][1]->Clone(Form("%s_uncertainty", meanRjg[coll][1]->GetName() ) );
     meanRjg[coll][2]->Divide( meanRjg[coll][0] );
     
   }
   
-  TFile * fSysResults = new TFile("relativeSys_dueTo_energyScalePlus.root","recreate");
-  //TFile * fSysResults = new TFile("relativeSys_dueTo_energyScaleMinus.root","recreate");
-  //  TFile * fSysResults = new TFile("relativeSys_dueTo_jetResolution.root","recreate");
-  // TFile * fSysResults = new TFile("relativeSys_dueTo_isolation.root","recreate");
+  TString outname = fname2.ReplaceAll("resultHistograms", "uncertainty");
+  TFile * fSysResults = new TFile(outname,"recreate");
 
   
   for ( int coll = 1 ; coll<=4 ; coll++) {   // On Sep 30, only pp and pbpb is studied.  pA will be added very soon        
     for ( int ipt = 1 ; ipt <=nPtBin ; ipt++) {
       dNdJetPt[coll][ipt][2]->Write();
+      dNdXjg[coll][ipt][2]->Write();
     }
     meanJetPt[coll][2]->Write();
+    meanXjg[coll][2]->Write();
     meanRjg[coll][2]->Write();
   }
   
---
+++ runGetUncercert.sh
@@ -0,0 +1,2 @@
+root -l -b -q 'getSysUncert.C+("resultHistograms.root","resultHistograms_jetEnegyScalePlus.root")'
+root -l -b -q 'getSysUncert.C+("resultHistograms.root","resultHistograms_jetEnegyScaleMinus.root")'
