82d0a2c6c3e0211b115d75501c83d5f1d5bcc130
Yongsun <kingmking@gmail.com>
2013-10-07 17:21:15 +0900

    updated runGetUncertainty.sh

---
+++ runGetUncertainty.sh
@@ -0,0 +1,3 @@
+root -l -b -q 'getSysUncert.C+("resultHistograms.root","resultHistograms_jetEnegyScalePlus.root")'
+root -l -b -q 'getSysUncert.C+("resultHistograms.root","resultHistograms_jetEnegyScaleMinus.root")'
+root -l -b -q 'getSysUncert.C+("resultHistograms.root","resultHistograms_jetResCorrection.root")'
