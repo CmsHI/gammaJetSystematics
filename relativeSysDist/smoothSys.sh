224733ae182d4203ce727c07ba77bd4ec476f1de
Yongsun <kingmking@gmail.com>
2013-11-12 15:10:07 +0900

    moved from the upper dif

---
+++ relativeSysDist/smoothSys.sh
@@ -0,0 +1,14 @@
+root -l   'smoothSys.C+("nominal")'
+
+root -l   'smoothSys.C+("photonEnergyScalePlus")'
+root -l   'smoothSys.C+("photonEnergyScaleMinus")'
+root -l   'smoothSys.C+("photonPurityVaried")'
+root -l   'smoothSys.C+("noElectronRejection")'
+root -l   'smoothSys.C+("noJetResCorr")'
+root -l   'smoothSys.C+("jetEnergyScalePlus")'
+root -l   'smoothSys.C+("jetEnergyScaleMinus")'
+root -l   'smoothSys.C+("MCrecoIso")'
+root -l   'smoothSys.C+("MCgenIso")'
+root -l   'smoothSys.C+("jetEnergySmeared")'
+
+#root -l   'smoothSys_20GeV.C("nominal_Jet20GeV")'
